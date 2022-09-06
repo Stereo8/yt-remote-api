1ec49b3b3ca11c2f1673c4cf378b83f8bbc62968
Vid Plavšić <plavsic.vid@gmail.com>
2022-09-06 11:51:07 +0200

    mongodb

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import express, { Express, Request, Response } from 'express'
+import mongoose from 'mongoose'
 import * as dotenv from 'dotenv'
 
 dotenv.config()
 
 const app = express()
+
+const dbConn = mongoose.connect(process.env.DB_URL)
+const db = mongoose.connection
+
+db.on('error', (error) => {
+  console.log(error)
+})
+
+db.once('connected', () => {
+  console.log('Database Connected')
+})
 
 const players = [0]
 
