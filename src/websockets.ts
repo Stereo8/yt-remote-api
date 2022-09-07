05dbc24d8a2e6827c2de3207f04c9765602f0bef
Vid Plavšić <plavsic.vid@gmail.com>
2022-09-07 21:49:59 +0200

    setup player ws connections

--- src/websockets.ts
+++ src/websockets.ts
@@ -1,10 +1,14 @@
-import ws from 'ws'
+import ws, { WebSocket } from 'ws'
+import queryString from 'query-string'
 import { Server } from 'http'
 
 export default (express: Server) => {
   const websocketServer = new ws.Server({
     noServer: true,
+    path: '/websockets',
   })
+
+  let playerConnections: Array<{ id: string; conn: WebSocket }> = []
 
   express.on('upgrade', (request, socket, head) => {
     websocketServer.handleUpgrade(request, socket, head, (websocket) => {
@@ -13,14 +17,31 @@ export default (express: Server) => {
   })
 
   websocketServer.on('connection', (conn, req) => {
-    conn.on('message', (message) => {
-      try {
-        const parsedMessage = JSON.parse(message.toString())
-        console.log(parsedMessage)
-      } catch (error) {
-        console.error('ws json parse fail')
+    const [_path, params] = req?.url?.split('?')
+    const wsParams = queryString.parse(params)
+
+    console.log(`new conn:\n  id: ${wsParams.id}\n  type: ${wsParams.type}`)
+
+    if (wsParams?.type && wsParams?.id) {
+      if (wsParams.type === 'player') {
+        conn.on('message', (data) => {
+          // TODO: send messages to connected remotes
+          console.log(`msg from ${wsParams.id} --- ${data}`)
+        })
+
+        conn.on('close', (code, reason) => {
+          console.log(`ws closed - ${code} - ${reason}`)
+          playerConnections = playerConnections.filter((pc) => {
+            return pc.id !== wsParams.id
+          })
+          console.log(playerConnections)
+        })
+
+        // @ts-ignore
+        playerConnections.push({ id: wsParams.id, conn })
+        console.log(playerConnections)
       }
-    })
+    }
   })
 
   return websocketServer
